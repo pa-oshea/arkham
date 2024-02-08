6bacb87d0f89f4517589405a3c9829f1960a55b2
Patrick O'Shea <p14oshea@gmail.com>
2024-02-08 18:59:02 +0000

    Parse let statments

--- go.mod
+++ go.mod
@@ -2,10 +2,7 @@ module arkham
 
 go 1.22.0
 
-require (
-	github.com/magiconair/properties v1.8.7
-	github.com/stretchr/testify v1.8.4
-)
+require github.com/stretchr/testify v1.8.4
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
---
+++ parser/parser_test.go
@@ -0,0 +1,58 @@
+package parser
+
+import (
+	"arkham/ast"
+	"arkham/lexer"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+	let x = 5;
+	let y = 10;
+	let foobar = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	require.NotNil(t, program, "ParseProgram return nil")
+	require.Len(t, program.Statements, 3)
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+		return false
+	}
+	return true
+}
