d4a9cae8b50b4d44f5b2d70f8e45c8a13b468013
Patrick O'Shea <p14oshea@gmail.com>
2024-02-13 19:22:40 +0000

    expanding repl

--- repl/repl.go
+++ repl/repl.go
@@ -2,7 +2,7 @@ package repl
 
 import (
 	"arkham/lexer"
-	"arkham/token"
+	"arkham/parser"
 	"bufio"
 	"fmt"
 	"io"
@@ -14,7 +14,7 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Printf(PROMPT)
+		fmt.Print(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
@@ -22,9 +22,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
