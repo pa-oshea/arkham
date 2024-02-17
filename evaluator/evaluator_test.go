0b76d1d25194fa8f421f42fa0750d282ad0bb9b1
Patrick O'Shea <p14oshea@gmail.com>
2024-02-17 16:57:51 +0000

    eval: Functions

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -5,6 +5,8 @@ import (
 	"arkham/object"
 	"arkham/parser"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestEvalIntegerExpression(t *testing.T) {
@@ -214,6 +216,52 @@ func TestLetStatement(t *testing.T) {
 	for _, tt := range tests {
 		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
+}
+
+func TestFunctionObject(t *testing.T) {
+	input := "fn(x) { x + 2; };"
+
+	evaluated := testEval(input)
+	fn, ok := evaluated.(*object.Function)
+	require.True(t, ok, "object is not a Functino. got=%T (%+v)", evaluated, evaluated)
+
+	require.Len(t, fn.Parameters, 1, "function has wrong number of parameters")
+
+	require.Equal(t, "x", fn.Parameters[0].String(), "parameter is not 'x'. got=%q", fn.Parameters[0])
+
+	expectedBody := "(x + 2)"
+
+	require.Equal(t, expectedBody, fn.Body.String())
+}
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x) { x; }; identity(5);", 5},
+		{"let identity = fn(x) { return x; }; identity(5);", 5},
+		{"let double = fn(x) { x * 2; }; double(5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5, 5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5 + 5, add(5, 5));", 20},
+		{"fn(x) { x; }(5)", 5},
+	}
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
+func TestClosures(t *testing.T) {
+	input := `
+	let newAdder = fn(x) {
+		fn(y) { x + y };
+	}
+
+	let addTwo = newAdder(2);
+	addTwo(2);
+	`
+
+	testIntegerObject(t, testEval(input), 4)
 }
 
 func testEval(input string) object.Object {
